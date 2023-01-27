d62437e36ea0fb510ebfc73011ef49d943783a27
Shruthika29 <111460144+Shruthika29@users.noreply.github.com>
2023-01-27 17:19:20 -0600

    Delete minmaxage.py

--- minmaxage.py
+++
@@ -1,9 +0,0 @@
-# sorting the list of ages.
-ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-# print the sorted list of ages.
-ages.sort()
-print(ages)
-# print the minimum age in the list.
-print("Minimum age is :",min(ages))
-# print the maximum age in the lsit
-print("Maximum age is :",max(ages))
