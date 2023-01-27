6fec6d7216e87449c38397e8bde33d9e6501adcc
Shruthika29 <111460144+Shruthika29@users.noreply.github.com>
2023-01-27 17:22:22 -0600

    Add files via upload

---
+++ 1c.py
@@ -0,0 +1,6 @@
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# Calculating the median between the above list of ages.
+medianAge= len(ages)//2
+medianResult =(ages[medianAge]+ ages[~medianAge])/2
+# displaying the result.
+print("The median age of list ages is :" ,medianResult)
