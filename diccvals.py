dd4ed99b169083453501370178548179ca0b1dab
Shruthika29 <111460144+Shruthika29@users.noreply.github.com>
2023-01-27 17:18:24 -0600

    Delete diccvals.py

--- diccvals.py
+++
@@ -1,4 +0,0 @@
-print("OUTPUT :")
-std ={'first_name':'shruthika', 'last_name':'veeravarapu', 'gender':'female','age': '24','marital status':'unmarried','skills':'being chill','country':'india','city':'hyderabad','address': 'telangana'}
-# printing student values from the list
-print("student values are:",std.values())
